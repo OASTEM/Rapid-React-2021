8641ed3e62b77a241d45592f5ae2348d28bece5a
rice31415 <ericwang3.1415@gmail.com>
2022-02-28 16:54:37 -0800

    Update ColorSorter.java

--- src/main/java/frc/robot/subsystems/ColorSorter.java
+++ src/main/java/frc/robot/subsystems/ColorSorter.java
@@ -8,12 +8,18 @@ import com.revrobotics.ColorMatch;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
 
 public class ColorSorter extends SubsystemBase {
   private final Color blueBall = ColorMatch.makeColor(0.185, 0.420, 0.395); 
   private final Color redBall = ColorMatch.makeColor(0.627, 0.310, 0.063);
   /** Creates a new ColorSorter. */
-  public ColorSorter() {}
+  public ColorSorter() {
+    m_colorMatcher.addColorMatch(blueBall);
+    m_colorMatcher.addColorMatch(redBall);
+  }
 
   @Override
   public void periodic() {
