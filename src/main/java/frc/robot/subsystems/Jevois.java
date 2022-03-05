163e0d527fb539c16b31a1ab3d238ee77bca09c5
Sanath Nair <sanathnair09@gmail.com>
2022-03-05 11:04:42 -0800

    fixed dependecy issues

--- src/main/java/frc/robot/subsystems/Jevois.java
+++ src/main/java/frc/robot/subsystems/Jevois.java
@@ -3,35 +3,24 @@ package frc.robot.subsystems;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoMode;
-import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoMode;
+
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Jevois extends SubsystemBase {
-  /**
-   * Creates a new Jevois.
-   */
-  private double depth = 0.0;
-  private double offsetAngle = 0.0;
 
   private SerialPort camPort;
+  private UsbCamera jevoisUSBCamera;
 
   public Jevois() {
-    UsbCamera jevoisUSBCamera = new UsbCamera("cam0", 1);
-    CameraServer jevoisCamServer = CameraServer.getInstance();
-    jevoisUSBCamera = jevoisCamServer.startAutomaticCapture();
-
+    jevoisUSBCamera = new UsbCamera("cam0", 1);
     jevoisUSBCamera.setVideoMode(VideoMode.PixelFormat.kYUYV, 640, 480, 20);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    //backgroundUpdate();
-    //System.out.println("depth: " + getdepth());
-    //System.out.println("Offset Angle: " +getOffsetAngle());
   }
 
   public void initializeSerialPort() {
@@ -76,26 +65,15 @@ public class Jevois extends SubsystemBase {
 
       if (data.length() > 0) {
         jsonData = (JSONObject) parser.parse(data);
-
-        if (jsonData.size() == 1) {
-          depth = -1;
-          offsetAngle = -1;
-        } else {
-          depth = (Double) jsonData.get("Depth");
-          offsetAngle = (Double) jsonData.get("Offset_angle");
-        }
+        System.out.println(jsonData.toString());
       }
     } catch (Exception e) {
       System.out.println("Error: " + e);
     }
   }
 
-  public double getDepth() {
-    return depth;  
-  }
-
-  public double getOffsetAngle() {
-    return offsetAngle;
+  public void destroy() {
+    jevoisUSBCamera.close();
   }
 
 }
