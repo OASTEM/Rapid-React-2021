e9dbeca9d03b775c4af58d5bbbfc3b586b1f1ef9
Shawn <shawngazin@protonmail.com>
2022-03-05 14:35:40 -0800

    Update TalonSRXMotor.java

--- src/main/java/frc/robot/commands/TalonSRXMotor.java
+++ src/main/java/frc/robot/commands/TalonSRXMotor.java
@@ -21,7 +21,7 @@ public class TalonSRXMotor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    test(5);
+    test.set(ControlMode.PercentOutput, );
   }
 
   // Called once the command ends or is interrupted.
