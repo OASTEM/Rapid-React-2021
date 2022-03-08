fb748a5cd644ba3b011fa53ce7f87dde5dbbe8dc
Gabriel Han <gabrielhan104@gmail.com>
2022-03-07 23:19:38 -0800

    stuff

--- src/main/java/frc/robot/commands/CargoManipulation.java
+++ src/main/java/frc/robot/commands/CargoManipulation.java
@@ -43,7 +43,6 @@ public class CargoManipulation extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  
   public void initialize() {
 
     if (isIntaking == true) {
