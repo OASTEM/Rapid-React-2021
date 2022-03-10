548f7b2eb0fc07d52e25126c3aa4e1083e1767f7
Gabriel Han <gabrielhan104@gmail.com>
2022-03-10 10:02:31 -0800

    sht

--- src/main/java/frc/robot/subsystems/Jevois.java
+++ src/main/java/frc/robot/subsystems/Jevois.java
@@ -3,10 +3,12 @@ package frc.robot.subsystems;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.cscore.VideoMode;
 
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Jevois extends SubsystemBase {
@@ -14,14 +16,18 @@ public class Jevois extends SubsystemBase {
   /**
    *
    */
-  private static final int baudrate = 921600;
+  private static final int BAUDRATE = 921600;
   private SerialPort camPort;
   private UsbCamera jevoisUSBCamera;
+  private CameraServer jevoisCameraServer;
   public double x = -1;
   public double y = -1;
 
   public Jevois() {
     jevoisUSBCamera = new UsbCamera("cam0", 1);
+    jevoisCameraServer = CameraServer.getInstance();
+    jevoisUSBCamera = jevoisCameraServer.startAutomaticCapture();
+
     boolean setVidMode = jevoisUSBCamera.setVideoMode(VideoMode.PixelFormat.kYUYV, 640, 480, 20);
     System.out.println("setvidmode: " + setVidMode);
     System.out.println("INITALIZING JEVOIS ***** YAYAYAYAAYAYA");
@@ -29,7 +35,7 @@ public class Jevois extends SubsystemBase {
       System.out.println("1st Try");
       // 921600
 
-      camPort = new SerialPort(baudrate, SerialPort.Port.kUSB2);
+      camPort = new SerialPort(BAUDRATE, SerialPort.Port.kUSB2);
 
     } catch (Exception e) {
 
@@ -37,7 +43,7 @@ public class Jevois extends SubsystemBase {
 
       try {
 
-        camPort = new SerialPort(baudrate, SerialPort.Port.kUSB);
+        camPort = new SerialPort(BAUDRATE, SerialPort.Port.kUSB);
 
       } catch (Exception j) {
 
@@ -45,7 +51,7 @@ public class Jevois extends SubsystemBase {
 
           System.out.println("Error - 3rd Try");
 
-          camPort = new SerialPort(baudrate, SerialPort.Port.kUSB1);
+          camPort = new SerialPort(BAUDRATE, SerialPort.Port.kUSB1);
 
         } catch (Exception k) {
 
@@ -58,7 +64,7 @@ public class Jevois extends SubsystemBase {
   }
 
   @Override
-  public void periodic(){
+  public void periodic() {
     // String data = camPort.readString();
     // System.out.println(data);
     // JSONObject obj = new JSONObject(data);
@@ -77,16 +83,15 @@ public class Jevois extends SubsystemBase {
       // byte[] otherStuff = camPort.read(100);
       // System.out.println(otherStuff[0]);
 
-
       // if (data.length() > 0) {
-      //   jsonData = (JSONObject) parser.parse(data);
-      //   x = (Double) jsonData.get("x");
-      //   y = (Double) jsonData.get("y");
-      //   System.out.println("x is ");
-      //   System.out.println(x);
+      // jsonData = (JSONObject) parser.parse(data);
+      // x = (Double) jsonData.get("x");
+      // y = (Double) jsonData.get("y");
+      // System.out.println("x is ");
+      // System.out.println(x);
       // } else {
-      //   x = -1;
-      //   y = -1;
+      // x = -1;
+      // y = -1;
       // }
     } catch (Exception e) {
       x = -1;
@@ -96,27 +101,27 @@ public class Jevois extends SubsystemBase {
     }
   }
 
-  public double getY(){
+  public double getY() {
     return y;
   }
-  
-  public double getX(){
+
+  public double getX() {
     return x;
   }
   // public void backgroundUpdate() {
-  //   try {
-  //     String data = camPort.readString();
+  // try {
+  // String data = camPort.readString();
 
-  //     JSONParser parser = new JSONParser();
-  //     JSONObject jsonData;
+  // JSONParser parser = new JSONParser();
+  // JSONObject jsonData;
 
-  //     if (data.length() > 0) {
-  //       jsonData = (JSONObject) parser.parse(data);
-  //       System.out.println(jsonData.toString());
-  //     }
-  //   } catch (Exception e) {
-  //     System.out.println("Error: " + e);
-  //   }
+  // if (data.length() > 0) {
+  // jsonData = (JSONObject) parser.parse(data);
+  // System.out.println(jsonData.toString());
+  // }
+  // } catch (Exception e) {
+  // System.out.println("Error: " + e);
+  // }
   // }
 
   public void destroy() {
