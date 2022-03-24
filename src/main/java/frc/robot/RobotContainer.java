0fb805c4b1a1a2da151673123055d26b58a817ae
Rishthewizard <92958472+Rishthewizard@users.noreply.github.com>
2022-03-24 12:05:14 -0700

    added smart dashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ArcadeDrive;
@@ -67,6 +67,8 @@ public class RobotContainer {
 
     driveTrain.setDefaultCommand(new ArcadeDrive(driveTrain, drivePad));
     configureButtonBindings();
+    
+    SmartDashboard.putData("DriveModeChooser", chooser);
   }
 
   /**
